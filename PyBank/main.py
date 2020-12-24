6d55b6ce5dcdbf268eab0feeee1f59eab9fd6665
Hari Sharma <harishar@umich.edu>
2020-12-23 20:54:26 -0500

    PyBank updated to calculate average change. PyPoll rounding error fixed for Khan

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,28 +3,51 @@ import csv
 months = 0
 net_total = 0
 greatest_increase = 0
-#average_change = 0
+net_change_list =[]
+average_change = 0
+total_difference = 0
+difference = 0
+month_of_change = []
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
 with open(budget_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
     # test that code is accessing csv file
+    first_row = next(csvreader)
+    prev_net_change = int(first_row[1])
+    
     print(csvreader)
     for row in csvreader:
         months += 1
         net_total += int(row[1])
-        next_row = next(csvreader)
-              
+        # using next makes it skip ahead, so data is missing if I use this
+        
+        
+        net_change = int(row[1]) - prev_net_change
+        prev_net_change = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
     
         
+       # difference = int(next_row[1]) - int(row[1])
+        #print(difference)
+         #monthly_change = {row: difference}
+    #print(monthly_difference)
+
+average_change = round(sum(net_change_list) / len(net_change_list), 2)
+
+print(average_change)
     
-   # average_change = net_total / months ----this is not correct
-    print("Financial Analysis")
-    print("---------------------------")
-    print("Total Months: " + str(months))
-    print("Total: $" + str(net_total))
-   # print(greatest_increase_month)
-    print(greatest_increase)
-   # print("Average Change: " + str(average_change))
+# Create a list of the change between months. Fill this into a dictionary to get the month. 
+# Use append function to create the new list. Need to do math on the values ahead somehow
     
+
+# # average_change = net_total / months ---> this is not correct
+# print("Financial Analysis")
+# print("---------------------------")
+# print("Total Months: " + str(months))
+# print("Total: $" + str(net_total))
+# # print(greatest_increase_month)
+# print(greatest_increase)
+# # print("Average Change: " + str(average_change))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,7 +26,7 @@ with open(election_csv, "r") as csvfile:
             otooley_total_votes += 1
 
 
-khan_percent = round(((khan_total_votes / total_votes) * 100), 3)
+khan_percent = (khan_total_votes / total_votes) * 100
 correy_percent = round(((correy_total_votes / total_votes) * 100), 3)
 li_percent = round(((li_total_votes / total_votes) * 100), 3)
 otooley_percent = round(((otooley_total_votes / total_votes) * 100), 3)
@@ -45,7 +45,7 @@ print("Election Results")
 print("--------------------")
 print("Total Votes: " + str(total_votes))
 print("--------------------")
-print("Khan: " + str(khan_percent) + "%" + " ("+ str(khan_total_votes) + ")")
+#print("Khan: {:.3f}".format(khan_percent)+ "%" + " (" {khan_total_votes} + ")")
 print("Correy: " + str(correy_percent) + "%" + " (" + str(correy_total_votes) + ")")
 print("Li: " + str(li_percent) + "%" + " (" + str(li_total_votes)+ ")")
 print("O'Tooley: " + str(otooley_percent) + "%" + " ("+ str(otooley_total_votes)+ ")")
@@ -54,19 +54,25 @@ print("Winner: " + winner)
 print("--------------------")
 
 
-# Export analysis to text file 
-output_path = os.path.join("analysis", "election_analysis.txt")
-with open(output_path, "w") as txtfile:
-    txtfile.writelines(["Election Results" + "\n",
-    "--------------------" + "\n",
-    "Total Votes: " + str(total_votes) + "\n", 
-    "Khan: " + str(khan_percent) + "%" + " ("+ str(khan_total_votes) + ")" + "\n",
-    "Correy: " + str(correy_percent) + "%" + " (" + str(correy_total_votes) + ")" + "\n",
-    "Li: " + str(li_percent) + "%" + " (" + str(li_total_votes)+ ")" + "\n",
-    "O'Tooley: " + str(otooley_percent) + "%" + " ("+ str(otooley_total_votes)+ ")" + "\n",
-    "--------------------" + "\n",
-    "Winner: " + winner + "\n",
-    "--------------------" + "\n"
-     ])
-txtfile.close()
 
+print("Khan: {:.3f}".format(khan_percent) + "% (" + str(khan_total_votes) + ")")
+#print(('{:.2f}'.format(str(khan_percent))))
+
+
+# Export analysis to text file 
+# output_path = os.path.join("analysis", "election_analysis.txt")
+# with open(output_path, "w") as txtfile:
+#     txtfile.writelines(["Election Results" + "\n",
+#     "--------------------" + "\n",
+#     "Total Votes: " + str(total_votes) + "\n", 
+#     "Khan: " + '{:.2f}'.format(str(khan_percent) + "%" + " ("+ str(khan_total_votes) + ")" + "\n",
+#     "Correy: " + str(correy_percent) + "%" + " (" + str(correy_total_votes) + ")" + "\n",
+#     "Li: " + str(li_percent) + "%" + " (" + str(li_total_votes)+ ")" + "\n",
+#     "O'Tooley: " + str(otooley_percent) + "%" + " ("+ str(otooley_total_votes)+ ")" + "\n",
+#     "--------------------" + "\n",
+#     "Winner: " + winner + "\n",
+#     "--------------------" + "\n"
+#      ])
+# txtfile.close()
+
+#'{:.2f}'.format(number)
