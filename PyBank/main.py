8beffaf9e730b251875d7a493d4914b680cad2f6
Hari Sharma <harishar@umich.edu>
2020-12-25 00:10:18 -0500

    Added greatest months and print to analysis file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,8 +2,8 @@ import os
 import csv
 months = 0
 net_total = 0
-greatest_increase = []
-greatest_decrease = []
+greatest_increase = 0
+greatest_decrease = 0
 net_change_list =[]
 average_change = 0
 total_difference = 0
@@ -30,42 +30,38 @@ with open(budget_csv, "r") as csvfile:
         prev_net_change = int(row[1])
         net_change_list += [net_change]
         month_of_change += [row[0]]
-
         
-        if net_change > greatest_increase[1]:
-            greatest_increase[0] = row[0]
-            greatest_increase[1] = row[1]
 
+        if net_change > greatest_increase:
+            greatest_increase_month = row[0]
+            greatest_increase = net_change
 
+        if net_change < greatest_decrease:
+            greatest_decrease_month = row[0]
+            greatest_decrease = net_change
     
-       # difference = int(next_row[1]) - int(row[1])
-        #print(difference)
-         #monthly_change = {row: difference}
-    #print(monthly_difference)
-
-# monthly_difference["key1"] = [month_of_change]
-# monthly_difference["key2"] = [net_change_list]
-
-# print(monthly_difference)
-
 
 average_change = round(sum(net_change_list) / len(net_change_list), 2)
-# monthly_difference = {month_of_change: net_change_list}
-#greatest_increase = max(monthly_difference, key=monthly_difference.get)
-
-print(greatest_increase)
-
-    
-# Create a list of the change between months. Fill this into a dictionary to get the month. 
-# Use append function to create the new list. Need to do math on the values ahead somehow
-    
-
 
 print("Financial Analysis")
 print("---------------------------")
 print("Total Months: " + str(months))
 print("Total: $" + str(net_total))
 print("Average Change: $" + str(average_change))
-# # print(greatest_increase_month)
-# print(greatest_increase)
-# # print("Average Change: " + str(average_change))
+print("Greatest Increase in Profits: " + greatest_increase_month + " ($" + str(greatest_increase) + ")")
+print("Greatest Decrease in Profits: " + greatest_decrease_month + " ($" + str(greatest_decrease) + ")")
+
+
+
+# Export analysis to text file 
+output_path = os.path.join("analysis", "bank_analysis.txt")
+with open(output_path, "w") as txtfile:
+    txtfile.writelines(["Financial Analysis \n",
+    "--------------------------- \n",
+    "Total Months: " + str(months) + "\n", 
+    "Total: $" + str(net_total) + "\n",
+    "Average Change: $" + str(average_change) + "\n",
+    "Greatest Increase in Profits: " + greatest_increase_month + " ($" + str(greatest_increase) + ") \n",
+    "Greatest Decrease in Profits: " + greatest_decrease_month + " ($" + str(greatest_decrease) + ") \n",
+     ])
+txtfile.close()
