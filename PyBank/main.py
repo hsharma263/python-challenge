4af93fc0721775120918862be134c8aa7b194d64
Hari Sharma <harishar@umich.edu>
2020-12-25 15:11:32 -0500

    Cleaning up code. Added more comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,6 @@ greatest_increase = 0
 greatest_decrease = 0
 net_change_list =[]
 average_change = 0
-total_difference = 0
-difference = 0
 month_of_change = []
 monthly_difference = {}
 budget_csv = os.path.join("Resources", "budget_data.csv")
@@ -16,7 +14,7 @@ with open(budget_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
     first_row = next(csvreader)
-    prev_net_change = int(first_row[1])
+    prev_total = int(first_row[1])
     months += 1
     net_total += int(first_row[1])
 
@@ -24,10 +22,8 @@ with open(budget_csv, "r") as csvfile:
     for row in csvreader:
         months += 1
         net_total += int(row[1])
-        
-        
-        net_change = int(row[1]) - prev_net_change
-        prev_net_change = int(row[1])
+        net_change = int(row[1]) - prev_total
+        prev_total = int(row[1])
         net_change_list += [net_change]
         month_of_change += [row[0]]
         
@@ -43,6 +39,7 @@ with open(budget_csv, "r") as csvfile:
 
 average_change = round(sum(net_change_list) / len(net_change_list), 2)
 
+# Print results to terminal 
 print("Financial Analysis")
 print("---------------------------")
 print("Total Months: " + str(months))
@@ -50,8 +47,6 @@ print("Total: $" + str(net_total))
 print("Average Change: $" + str(average_change))
 print("Greatest Increase in Profits: " + greatest_increase_month + " ($" + str(greatest_increase) + ")")
 print("Greatest Decrease in Profits: " + greatest_decrease_month + " ($" + str(greatest_decrease) + ")")
-
-
 
 # Export analysis to text file 
 output_path = os.path.join("analysis", "bank_analysis.txt")
